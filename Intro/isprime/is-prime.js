431fe66b1b87edfddb6900e369c98c727a3a0566
panamabilly <billsweeney85@gmail.com>
2022-03-22 10:10:26 -0500

    Challenge complete is-prime.js

--- Intro/isprime/is-prime.js
+++ Intro/isprime/is-prime.js
@@ -1,10 +1,19 @@
 const isPrime = (n) => {
-	// code only works for positive integers
-	if (n > 0) {
-		
+	// The number 1 can not be prime therefore return false
+	if (n < 2) {
+		return false;
 	}
+	for (let i = 2; i < n; i++) {
+		if (n % i === 0) {
+			return false; // loop through all numbers between 2 and n checking to see if i can divide into n, If it can then return false because number is not prime
+		}
+	}
+	return true; // since we checked all the cases in the for loop above return true because if not false the number is prime.
 };
+
+console.log(isPrime(7));
 
 module.exports = {
 	isPrime,
 };
+ 
